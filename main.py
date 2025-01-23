3efb2dc7fdab800dd3f07c253eb87f94c3f79665
Fachry Rahardhian saputra <fachrysaputra2@gmail.com>
2025-01-24 00:06:50 +0700

    final versi 1

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.lang.builder import Builder
-from kivy.properties import StringProperty, NumericProperty, ObjectProperty, BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty
+from kivy.core.audio import SoundLoader
 from functools import partial
+from kivy.clock import Clock
+from kivy.graphics import Rectangle
 
 import random
 import string
@@ -19,6 +22,7 @@ Builder.load_file('homepage.kv')
 Builder.load_file('levelpage.kv')
 Builder.load_file('gamepage.kv')
 Builder.load_file('temapage.kv')
+Builder.load_file('skor_page.kv')
 
 #json reader
 def read_json_file(file_path):
@@ -29,14 +33,33 @@ def read_json_file(file_path):
 
 #main app runner 
 class MyApp(App):
+    soundbgm = SoundLoader.load('assets/sounds/bgm.mp3')
+
     def build(self):
+        self.playBGM()
         sm = ScreenManager()
         sm.transition = FadeTransition()
         sm.add_widget(HomePage())
         sm.add_widget(LevelPage())
         sm.add_widget(GamePage())
         sm.add_widget(TemaPage())
+        sm.add_widget(SkorPage())
         return sm
+    
+
+    def playBGM(self):
+        if(self.soundbgm):
+            self.soundbgm.loop = True
+            self.soundbgm.play()
+
+class SkorPage(Screen):
+    skor = None
+
+    def set_score(self,arg_score):
+        self.skor = arg_score
+        angka_skor = self.ids.angka_skor
+        angka_skor.text = str(self.skor)
+        
 
 class HomePage(Screen):
     pass
@@ -48,13 +71,14 @@ class LevelPage(Screen):
 
 
 class GamePage(Screen):
-    arg_tema = StringProperty("")
-    arg_level = BooleanProperty(False)
+    arg_tema = None 
+    arg_level = None
     currentSoalPicture = ""
     currentTheme = []
     currentIndexSoal = 0
     currentAnswer = []
     currentRandomOptions = []    
+    totalPoint = 0
 
     def set_arg(self,tema,level):
         self.arg_level = level
@@ -87,54 +111,36 @@ class GamePage(Screen):
     def init_game(self):
         self.db = read_json_file("db.json")
         self.currentTheme = self.db[self.arg_tema]
-        print(self.arg_tema)
-
+        self.totalPoint = 0
+        self.currentIndexSoal = 0
         random.shuffle(self.currentTheme)
+        # SET jumlah soal disini
         self.currentTheme = self.currentTheme[:10]    
+        print("INISIASI")
+        self.setupSoal()
 
-        # self.currentSoalName = self.currentTheme[self.currentIndexSoal]['name']
-        # self.currentSoalPicture = f"db/images/{self.currentTheme[self.currentIndexSoal]['source']}"
-        # self.currentSoalSound = f"db/sound/{self.currentTheme[self.currentIndexSoal]['sound']}"
+    def go_to_score_page(self,nan):
+        print(f"TOTAL POIN : {self.totalPoint}")
+        self.manager.get_screen('score_page').set_score(self.totalPoint)
+        self.manager.current = 'score_page'   
 
-        # randomed = self.randomize(self.currentSoalName)
-        # self.currentAnswer = list(' '*len(self.currentSoalName))
-        # self.currentRandomOptions = list(randomed)
-        
-        # image_soal = self.ids.image_soal
-        # image_soal.source = self.currentSoalPicture
-        self.setupSoal(0)
-
-        # answer_container = self.ids.answer_container
-        # answer_container.clear_widgets()
-        # answer_container.cols = len(self.currentAnswer)
-        
-        # options_container = self.ids.option_container
-        # options_container.clear_widgets()    
-        # for i in range(1, len(self.currentAnswer) + 1):
-        #     box = AnswerBox()
-        #     box.label_text = ' '
-        #     answer_container.add_widget(box)
-        # for x in range(1, len(self.currentRandomOptions)+ 1):
-        #     box = RandomAnswer()
-        #     box.label_text = self.currentRandomOptions[x-1] 
-        #     box.on_press = partial(self.hit_answer,x-1)
-        #     options_container.add_widget(box)
-
-
-    def delete_answer(self):
-        yes = 0 
+    def update_score(self):
+        score_label = self.ids.score_label
+        score_label.text = f"POIN : {self.totalPoint}"    
 
     def check_answer(self):
         print(self.currentAnswer)
         print(list(self.currentSoalName))
         if(self.currentAnswer == list(self.currentSoalName)):
-                print("YEAH")     
+                self.totalPoint = self.totalPoint + 100
+                self.update_score() 
                 self.next_soal()       
         else:
             print("FLASE")
 
 
-    def setupSoal(self,index):
+    def setupSoal(self):
+        index = self.currentIndexSoal
         self.currentSoalName = self.currentTheme[index]['name']
         self.currentSoalPicture = f"db/images/{self.currentTheme[index]['source']}"
         self.currentSoalSound = f"db/sound/{self.currentTheme[index]['sound']}"
@@ -143,6 +149,21 @@ class GamePage(Screen):
         self.currentAnswer = list(' '*len(self.currentSoalName))
         self.currentRandomOptions = list(randomed)
         
+        self.update_score()
+        if (self.arg_level):
+            sound_button = Button(
+                size=(150, 150),
+                size_hint=(None, None),
+                allow_stretch=False,
+                keep_ratio=True,
+                pos_hint={'x': 0.7, 'y': 0.5},
+                background_normal='assets/buttons/nav/sound.png',
+                background_down='assets/buttons/nav/sound.png',
+                border=[0, 0, 0, 0]
+            )
+            sound_button.bind(on_press= lambda instance: self.play_sound(self.currentSoalSound))
+            self.add_widget(sound_button)
+
         image_soal = self.ids.image_soal
         image_soal.source = self.currentSoalPicture
 
@@ -160,20 +181,29 @@ class GamePage(Screen):
             box = RandomAnswer()
             box.label_text = self.currentRandomOptions[x-1] 
             box.on_press = partial(self.hit_answer,x-1)
-            options_container.add_widget(box)
+            options_container.add_widget(box)        
 
     def next_soal(self):
         if(self.currentIndexSoal < 9):
             print("GANTI SOAL")
             self.currentIndexSoal = self.currentIndexSoal + 1
-            self.setupSoal(self.currentIndexSoal)
+            self.setupSoal()
         else:
            print("SELESAI")
-           print("Ke halaman point akhir")    
+           self.play_sound('assets/sounds/yay.mp3')
+        #    self.add_widget(WinDecoration())
+           Clock.schedule_once(self.go_to_score_page, 2)
             
 
+    def play_sound(self,source):
+        sound = SoundLoader.load(source)
+        if(sound):
+            sound.play()
 
+    def go_to_home(self):
+        self.manager.current = 'home_page'
 
+            
     def hit_answer(self,index):
         if((sum(1 for item in self.currentAnswer if item == ' ') != 0)):  
         #    jika masih bisa menjawab
@@ -196,32 +226,7 @@ class GamePage(Screen):
                 options_container.add_widget(box)
             self.check_answer()         
         else:
-            print("")    
-    # def create_options(self):
-        # options_container = self.ids.option_container
-        # options_container.clear_widgets()
-        # ans = "MOBIL"
-        # # Mengacak huruf dalam kalimat
-        # kalimat_acak = ''.join(random.sample(ans, len(ans)))
-        
-        # # Menentukan jumlah huruf tambahan (antara 3 hingga 4)
-        # jumlah_tambahan = random.randint(3, 4)
-        
-        # # Membuat huruf tambahan yang tidak ada di kalimat input
-        # huruf_tambahan = ''.join(random.choices(
-        #     [c for c in string.ascii_uppercase if c not in ans], 
-        #     k=jumlah_tambahan
-        # ))
-        
-        # # Menyisipkan kalimat acak di posisi acak dalam huruf tambahan
-        # posisi = random.randint(0, len(huruf_tambahan))  # Posisi sisipan acakan
-        # campuran = kalimat_acak + huruf_tambahan
-        # # randomed = huruf_tambahan[:posisi] + kalimat_acak + huruf_tambahan[posisi:]
-        # randomed = ''.join(random.sample(campuran,len(campuran)))
-        # for i in range(1, len(randomed) + 1):
-        #     box = RandomAnswer()
-        #     box.label_text = randomed[i-1] 
-        #     options_container.add_widget(box)   
+            print("")  
     
     
 
@@ -232,6 +237,8 @@ class TemaPage(Screen):
         self.arg_level = level
     
     def gotoGamePage(self,tema):
+        self.manager.remove_widget(self.manager.get_screen('game_page'))
+        self.manager.add_widget(GamePage())
         self.manager.get_screen('game_page').set_arg(tema,self.arg_level)
         self.manager.current = 'game_page'
 
@@ -242,6 +249,9 @@ class AnswerBox(Button):
 
 class RandomAnswer(Button):
     label_text = StringProperty('');
+
+class WinDecoration(Rectangle):
+    pass
 
 class OptionLabel(Label):
     pass
